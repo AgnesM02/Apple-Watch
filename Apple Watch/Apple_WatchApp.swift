bcca48a9b93bd75500ef12b439d2abb1abd91655
AgnesM02 <62841666+AgnesM02@users.noreply.github.com>
2022-09-28 18:16:07 +0800

    Add files via upload

---
+++ Apple Watch/Apple_WatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_WatchApp.swift
+//  Apple Watch
+//
+//  Created by User11 on 2022/9/21.
+//
+
+import SwiftUI
+
+@main
+struct Apple_WatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
